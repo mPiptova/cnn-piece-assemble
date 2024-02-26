8c38d79a5b873dddf6898e8053ea5a1f79b5efee
Marcela Brichtova Piptova <marcela.brichtova.piptova@rossum.ai>
2024-02-26 20:02:05 +0100

    Add contour smoothing function

--- src/piece_assemble/contours.py
+++ src/piece_assemble/contours.py
@@ -1,5 +1,6 @@
 import cv2
 import numpy as np
+from scipy.ndimage import gaussian_filter1d
 
 from piece_assemble.types import BinImg, Points
 
@@ -38,3 +39,27 @@ def extract_contours(img_bin: BinImg) -> tuple[Points, list[Points]]:
     return contours_to_points(outer_contour), [
         contours_to_points(contours) for contours in holes_contours
     ]
+
+
+def smooth_contours(contours: Points, sigma: float) -> Points:
+    """Smooth contour curve with gaussian filter.
+
+    Parameters
+    ----------
+    contours
+        2d array of points
+    sigma
+        Size of the gaussian filter
+
+    Returns
+    -------
+    smoothed_contours
+        2d array of points
+    """
+    return np.stack(
+        [
+            gaussian_filter1d(contours[:, 0].astype(float), sigma, mode="wrap"),
+            gaussian_filter1d(contours[:, 1].astype(float), sigma, mode="wrap"),
+        ],
+        axis=1,
+    )
