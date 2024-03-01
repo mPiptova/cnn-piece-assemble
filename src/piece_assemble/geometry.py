6d9d4f335fea2219654dd7281d0052f26d777580
Marcela Brichtova Piptova <marcela.brichtova.piptova@rossum.ai>
2024-03-01 11:51:30 +0100

    Add function for computing point distances

--- src/piece_assemble/geometry.py
+++ src/piece_assemble/geometry.py
@@ -23,3 +23,23 @@ def point_to_line_dist(points: Points, line_segment: tuple[Point, Point]):
     return (
         (b[0] - a[0]) * (a[1] - points[:, 1]) - (b[1] - a[1]) * (a[0] - points[:, 0])
     ) / segment_length
+
+
+def points_dist(points1: Point, points2: Points) -> np.ndarray[float]:
+    """Return distances of all point pairs from given set of points.
+
+    Parameters
+    ----------
+    points1
+        2d array where rows represent points (x, y).
+    points2
+        2d array where rows represent points (x, y).
+
+    Returns
+    -------
+    distances
+        2d array where distance of `points1[i]` and `points2[j]` is stored at position
+        `[i, j]`.
+    """
+    diff = points1[:, np.newaxis, :] - points2[np.newaxis, :, :]
+    return np.linalg.norm(diff, axis=2)
