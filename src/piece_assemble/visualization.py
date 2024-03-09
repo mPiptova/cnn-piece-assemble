2fd8ca7f185c5e81e6266458cd64889f2064ec2b
Marcela Brichtova Piptova <marcela.brichtova.piptova@rossum.ai>
2024-03-09 21:10:17 +0100

    Create module for visualization

---
+++ src/piece_assemble/visualization.py
@@ -0,0 +1,119 @@
+import numpy as np
+from PIL import ImageDraw
+from PIL.Image import Image as PilImage
+
+from piece_assemble.image import np_to_pil
+from piece_assemble.types import NpImage, Point, Points
+
+
+def draw_contour(
+    contour: Points, contours_img: NpImage = None, value: float = 0
+) -> NpImage:
+    """Draw contours in an image represented as numpy array
+
+    Parameters
+    ----------
+    contour
+        2d array of all points representing a shape contour.
+    contours_img
+        Image represented as numpy array where the contours will be drawn.
+        If None, new array with the ideal shape will be created.
+    value
+        Color of drawn contours. 0 corresponds to black, 1 to white.
+
+    Returns
+    -------
+    Image represented as numpy array.
+    """
+    contour = np.round(contour).astype(int)
+    # Shift coordinates to eliminate empty space
+    min = np.min(contour, axis=0)
+    contour = contour - min
+
+    # If image is not given, create empty one
+    if contours_img is None:
+        contours_img = np.ones((contour[:, 0].max() + 1, contour[:, 1].max() + 1))
+    contours_img[contour[:, 0], contour[:, 1]] = value
+    return contours_img
+
+
+def draw_circle_arc(
+    radius: float,
+    center: Point,
+    draw: ImageDraw,
+    color: int = 128,
+    angle_range: tuple[int, int] = None,
+) -> None:
+    """Draw a circle arc given by radius, center and an angle range.
+
+    Parameters
+    ----------
+    radius
+        Radius of the circle.
+    center
+        Center of the circle, given as an array [row_coordinate, col_coordinate]
+    draw
+        ImageDraw instance where the arc should be drawn.
+    color
+        Outline color, 0 is black, 255 is white.
+    angle_range
+        Specifies which part of the circle should be drawn. A tuple of two angles in
+        degrees.
+        If None, whole circle is drawn.
+    """
+    leftUpPoint = (center[1] - radius, center[0] - radius)
+    rightDownPoint = (center[1] + radius, center[0] + radius)
+    twoPointList = [leftUpPoint, rightDownPoint]
+    if angle_range is None:
+        draw.ellipse(twoPointList, outline=color)
+    else:
+        draw.arc(twoPointList, start=angle_range[0], end=angle_range[1], fill=color)
+
+
+def draw_circle_approximation(
+    contour: Points,
+    circle_arcs: list[int, tuple[int, int]],
+    centers: list[int],
+    radii: list[int],
+) -> PilImage:
+    """Draw a curve approximated by circle arcs.
+
+    Parameters
+    ----------
+    contour
+        2d array of all points representing a shape contour.
+    circle_arcs
+        Description of approximating circle arcs.
+        A list of tuples `(i, (start, end))` where `i` is the index of a point in
+        `contour`, to which the approximating osculating circle belongs to.
+        `(start, end)` describes which part of `contour` is well approximated by this
+        arc.
+    centers
+        Centers of all osculating circles of `contour` points.
+    radii
+        Radii of all osculating circles of `contour` points.
+
+    Returns
+    -------
+    An image of a given curve approximated by osculating circles.
+
+    """
+    img = np.ones((int(contour[:, 0].max()) + 1, int(contour[:, 1].max()) + 1))
+    img = np_to_pil(img)
+    draw = ImageDraw.Draw(img)
+
+    for i, interval in circle_arcs:
+        center = centers[i]
+        radius = np.abs(radii[i])
+        point_start, point_end = contour[interval[0]], contour[interval[1]]
+
+        alpha_start = np.rad2deg(np.arctan2(*(point_start - center))) % 360
+        alpha_end = np.rad2deg(np.arctan2(*(point_end - center))) % 360
+
+        if radii[i] > 0:
+            arc = (alpha_end, alpha_start)
+        else:
+            arc = (alpha_start, alpha_end)
+
+        draw_circle_arc(radius, center, draw, color=100, angle_range=arc)
+    return img
