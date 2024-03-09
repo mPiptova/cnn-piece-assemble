79da283669fc5fba611228e44d544e50233f4846
Marcela Brichtova Piptova <marcela.brichtova.piptova@rossum.ai>
2024-03-09 20:46:21 +0100

    Add more type definitions

--- src/piece_assemble/types.py
+++ src/piece_assemble/types.py
@@ -1,7 +1,12 @@
 import numpy as np
+from PIL.Image import Image
 
 Point = np.ndarray[float]
 Points = np.ndarray[float]
+
+# Image
 BinImg = np.ndarray[bool]
+NpImage = np.ndarray[float]
+PilImage = Image
 
 Interval = tuple[int, int] | tuple[float, float]
