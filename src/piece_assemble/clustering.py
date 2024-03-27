1bc81aa66444669a464c3ff171fec70bb3c21f00
Marcela Brichtova Piptova <marcela.brichtova.piptova@rossum.ai>
2024-03-27 18:28:39 +0100

    Implement Cluster representation

---
+++ src/piece_assemble/clustering.py
@@ -0,0 +1,68 @@
+from __future__ import annotations
+
+from functools import cached_property
+from itertools import combinations
+
+import shapely
+
+from piece_assemble.geometry import Transformation
+from piece_assemble.osculating_circle_descriptor import OsculatingCircleDescriptor
+
+
+class Cluster:
+    def __init__(
+        self, pieces: dict[str, tuple[OsculatingCircleDescriptor, Transformation]]
+    ) -> None:
+        self._pieces = pieces
+
+        self.descriptors = {key: desc for key, (desc, _) in pieces.items()}
+        self.transformations = {key: t for key, (_, t) in pieces.items()}
+
+    @cached_property
+    def border_length(self) -> int:
+        raise NotImplementedError()
+
+    @property
+    def pieces_ids(self) -> set[str]:
+        return set(self._pieces.keys())
+
+    def copy(self):
+        return Cluster(self._pieces.copy())
+
+    def transform(self, transformation: Transformation) -> Cluster:
+        new_pieces = {
+            key: (desc, t.compose(transformation))
+            for key, (desc, t) in self._pieces.items()
+        }
+        return Cluster(new_pieces)
+
+    @cached_property
+    def self_intersection(self) -> float:
+        polygons = [
+            shapely.transform(desc._polygon, lambda pol: t.apply(pol))
+            for desc, t in self._pieces.values()
+        ]
+        return max(
+            [
+                p1.intersection(p2).area / min(p1.area, p2.area)
+                for p1, p2 in combinations(polygons, 2)
+            ]
+        )
+
+    def merge(self, other: Cluster) -> Cluster:
+        common_keys = self.pieces_ids.intersection(other.pieces_ids)
+
+        common_key = common_keys.pop()
+        cluster1 = self.transform(self.transformations[common_key].inverse())
+        cluster2 = other.transform(other.transformations[common_key].inverse())
+
+        for key in common_keys:
+            if not cluster1.transformations[key].is_close(
+                cluster2.transformations[key]
+            ):
+                return None
+
+        new_cluster = Cluster(cluster1._pieces.update(cluster2._pieces))
+        if new_cluster.self_intersection > 0.1:
+            return None
+        return new_cluster
