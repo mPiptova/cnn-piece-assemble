239c2a5f2749c0ddcbc0881c4347c76f9970ca68
Marcela Brichtova Piptova <marcela.brichtova.piptova@rossum.ai>
2024-04-12 12:24:57 +0200

    Implement find_matches

--- src/piece_assemble/matching/__init__.py
+++ src/piece_assemble/matching/__init__.py
@@ -1,6 +1,14 @@
 from __future__ import annotations
 
+from itertools import combinations
+
+import numpy as np
+import pandas as pd
+
+from piece_assemble.clustering import Cluster
+from piece_assemble.descriptor import DescriptorExtractor
 from piece_assemble.matching.match import Match
+from piece_assemble.piece import Piece
 
 
 def filter_matches(matches: list[Match]):
@@ -28,3 +36,55 @@ def filter_matches(matches: list[Match]):
         filtered_matches.append(match)
 
     return filtered_matches
+
+
+def find_matches(
+    piece1: Piece, piece2: Piece, descriptor_extractor: DescriptorExtractor
+) -> list[Match]:
+    desc_dist = descriptor_extractor.dist(piece1.descriptor, piece2.descriptor)
+    len1 = piece1.get_segment_lengths()
+    len2 = piece2.get_segment_lengths()
+    max_len = np.maximum(len1[:, np.newaxis], len2[np.newaxis, :])
+    desc_dist = desc_dist / (10 * max_len)
+    flat_dist = desc_dist.flatten()
+    ordering = np.argsort(flat_dist)
+    idxs1, idxs2 = np.unravel_index(ordering[:30], desc_dist.shape)
+    return [
+        Match(
+            piece1.name,
+            piece2.name,
+            dist=desc_dist[i, j],
+            piece1=piece1,
+            piece2=piece2,
+            index1=i,
+            index2=j,
+        )
+        for i, j in zip(idxs1, idxs2)
+    ]
+
+
+def find_all_matches(
+    pieces: list[Piece], descriptor_extractor: DescriptorExtractor
+) -> list[Match]:
+    matches = []
+    for desc1, desc2 in combinations(pieces, 2):
+        matches.extend(find_matches(desc1, desc2, descriptor_extractor))
+    return matches
+
+
+def matches_to_df(matches: list[Match]) -> pd.DataFrame:
+    columns = ["key1", "key2", "dist", "match"]
+
+    def match_to_row(match: Match):
+        return (match.key1, match.key2, match.dist, match)
+
+    rows = [match_to_row(match) for match in matches]
+    matches_df = pd.DataFrame(rows, columns=columns)
+    matches_df = matches_df.sort_values("dist").reset_index()
+    return matches_df
+
+
+def matches_to_clusters(matches: list[Match]) -> list[Cluster]:
+    clusters = [match.to_cluster() for match in list(matches["match"])]
+    clusters.sort(key=lambda cluster: cluster.score, reverse=True)
+    return clusters
