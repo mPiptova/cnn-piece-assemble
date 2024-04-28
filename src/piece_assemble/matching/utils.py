4ad9fc7627f8d293c702648797b0a1dea70e17ea
Marcela Brichtova Piptova <marcela.brichtova.piptova@rossum.ai>
2024-04-28 09:52:40 +0200

    Refactopr get_initial_transformation

--- src/piece_assemble/matching/utils.py
+++ src/piece_assemble/matching/utils.py
@@ -5,10 +5,6 @@ from piece_assemble.piece import Piece
 def get_initial_transformation(
     piece1: Piece, piece2: Piece, idx1: int, idx2: int
 ) -> Transformation:
-    interval1 = piece1.segments[idx1].interval
-    interval2 = piece2.segments[idx2].interval
-
-    points1 = piece1.contour[interval1, :]
-    points2 = piece2.contour[interval2[::-1], :]
-
+    points1 = piece1.segments[idx1].contour[[0, -1]]
+    points2 = piece2.segments[idx2].contour[[-1, 0]]
     return fit_transform(points1, points2)
