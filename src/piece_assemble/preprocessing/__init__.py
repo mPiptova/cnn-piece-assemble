5813dbfdafd6205ba4120b48de3c4b329a314b17
Marcela Brichtova Piptova <marcela.brichtova.piptova@rossum.ai>
2025-01-11 11:44:12 +0000

    Remove preprocessing

--- src/piece_assemble/preprocessing/__init__.py
+++
@@ -1,7 +0,0 @@
-from piece_assemble.preprocessing.base import PieceExtractorBase
-from piece_assemble.preprocessing.negative import NegativePieceExtractor
-
-__all__ = [
-    "PieceExtractorBase",
-    "NegativePieceExtractor",
-]
