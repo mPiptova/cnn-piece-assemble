e63088391134f6e4a490133801a08760157241d6
Marcela Brichtova Piptova <marcela.brichtova.piptova@rossum.ai>
2025-03-14 14:50:15 +0000

    Add scrit evaluate_all.py

---
+++ src/piece_assemble/evaluate_all.py
@@ -0,0 +1,69 @@
+import argparse
+import os
+from copy import deepcopy
+
+from piece_assemble.config import load_config
+from piece_assemble.evaluation import cluster_to_transformations, evaluate
+from piece_assemble.load import load_puzzle
+from piece_assemble.tools.run import run_prediction
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Evaluate piece assemble.")
+    parser.add_argument("config", type=str, help="Path to the configuration file")
+    parser.add_argument("puzzles_path", type=str)
+    parser.add_argument("dataset_path", type=str)
+    args = parser.parse_args()
+
+    config_path = args.config
+    config = load_config(config_path)
+
+    with open(args.puzzles_path, "r") as f:
+        puzzle_dirs = f.readlines()
+        puzzle_dirs = [
+            os.path.join(args.dataset_path, puzzle_dir.strip())
+            for puzzle_dir in puzzle_dirs
+        ]
+
+    eval_results = {}
+
+    for puzzle_dir in puzzle_dirs:
+        pieces, neighbors = load_puzzle(puzzle_dir)
+        gt_transformations = {
+            piece.name: piece.transformation for piece in pieces.values()
+        }
+
+        copy_config = deepcopy(config)
+        copy_config["img_path"] = puzzle_dir
+        if config["logging"].get("output_images_path", False):
+            copy_config["logging"]["output_images_path"] = os.path.join(
+                config["logging"]["output_images_path"], puzzle_dir.split("/")[-1]
+            )
+
+        pred_clusters = [
+            cluster_to_transformations(cluster)
+            for cluster in run_prediction(copy_config)
+        ]
+        metrics = evaluate(
+            pred_clusters,
+            gt_transformations,
+            neighbors,
+            config["cluster"]["rotation_tol"],
+            config["cluster"]["translation_tol"],
+        )
+        print(puzzle_dir, metrics)
+
+        eval_results[puzzle_dir] = metrics
+
+    aggregated_metrics = {}
+    for metric in metrics.keys():
+        aggregated_metrics[metric] = sum(
+            [metrics[metric] for metrics in eval_results.values()]
+        ) / len(eval_results)
+
+    print(",".join(["puzzle"] + list(metrics.keys())))
+    for puzzle_dir, metrics in eval_results.items():
+        print(",".join([puzzle_dir] + [str(metrics[key]) for key in metrics.keys()]))
+
+    print(
+        ",".join(["aggregated"] + [str(value) for value in aggregated_metrics.values()])
+    )
